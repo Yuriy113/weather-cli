092129db93fbe10fa2e345db250c85e7c613f05b
Yuriy Ulanov <1859697@gmail.com>
2022-08-11 20:05:04 +0300

    environment environment

--- services/storage.service.js
+++ services/storage.service.js
@@ -4,11 +4,37 @@ import { promises } from 'fs';
 
 const filePath = join(homedir(), 'weather-data.json');
 
-const saveKeyValue = async (key, value) => {
-  const data = {};
-
-  data[key] = value;
-  await promises.writeFile(filePath, JSON.stringify(data));
+const TOKEN_DICTIONARY = {
+  token: 'token',
+  city:'city',
 }
 
-export { saveKeyValue };
+const saveKeyValue = async (key, value) => {
+  let data = {};
+  if (await isExict(filePath)) {
+    const file = await promises.readFile(filePath);
+    data = JSON.parse(file);
+  }
+  data[key] = value;
+  await promises.writeFile(filePath, JSON.stringify(data));
+};
+
+const getKeyValue = async (key) => {
+  if (await isExict(filePath)) {
+    const file = await promises.readFile(filePath);
+    const data = JSON.parse(file);
+    return data[key];
+  }
+  return undefined;
+}
+
+const isExict = async (path) => {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+export { saveKeyValue, getKeyValue, TOKEN_DICTIONARY };
--- weather.js
+++ weather.js
@@ -1,21 +1,36 @@
 #!/usr/bin/env node
 
 import { getArgs } from './helpers/args.js'; // импорт функции getArgs через ES modules
-import { printHelp } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { getWeather } from './services/api.service.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError('Не передан токен');
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
+    printSuccess('Токен сохранен');
+  } catch (e) {
+    printError(e.message);
+  }
+}
 
 const initCLI = () => { // создаем стрелочную функцию initCLI
   const args = getArgs(process.argv); // в переменную args присваиваем результат выполнения импортированной функции, с переданным в нее массивом process.argv
+  // console.log(process.env);
   if (args.h) {
     printHelp();
   }
   if (args.s) {
-    
+
   }
   if (args.t) {
-    saveKeyValue('token', args.t)
+    return saveToken(args.t);
   }
-
+  getWeather('moscow');
   // вывести погоду
 };
 
